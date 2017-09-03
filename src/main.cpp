389dd0898ae11e6636af7596231071b4790af9c2
Josh <joshjet@live.com>
2017-09-03 12:15:46 -0600

    Set up the PCM so to test the pedal position code. Updated gitignore to ignore emacs backups.

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,25 @@
 #include "WProgram.h"
+#include "read_pps.h"
 #include "logger.h"
 
 int main()
 {
+ 
+  //Set up resources:
   initialize_logging(DEBUG);
-  #include "read_pps.h"
-  
-   //Set up resources:
-   initialize_pps_read();
+  initialize_pps_read();
    
-   //Set up the LED pin as an output.
-   pinMode(13, OUTPUT);
+  //Set up the LED pin as an output.
+  pinMode(13, OUTPUT);
 	
-   //Control Loop:
-   while (1)
-     {
-       digitalWriteFast(13, HIGH);
-       delay(250);
-       digitalWriteFast(13, LOW);
-       delay(250);
-       String logpps = String("Pedal Position: ") + get_acc_pedal_position();
-       FSAE_log(DEBUG, logpps.c_str());
-     }
+  //Control Loop:
+  while (1)
+    {
+      digitalWriteFast(13, HIGH);
+      delay(250);
+      digitalWriteFast(13, LOW);
+      delay(250);
+      String logpps = String("Pedal Position: ") + get_acc_pedal_position();
+      FSAE_log(DEBUG, logpps.c_str());
+    }
 }
-// extern "C" int main(void)
-// {
-// #include "read_pps.h"
-  
-//   //Set up resources:
-//   initialize_pps_read();
-
-  
-//   //Set up the LED pin as an output.
-//   pinMode(13, OUTPUT);
-	
-//   //Control Loop:
-//   while (1)
-//     {
-//       digitalWriteFast(13, HIGH);
-//       delay(250);
-//       digitalWriteFast(13, LOW);
-//       delay(250);
-//       Serial.print("Pedal Position: "); 
-//       Serial.println(get_acc_pedal_position(), 20);
-//     }
-// }
-
